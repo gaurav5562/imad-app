7622a447a994738f7e42b927465bad732c41e321
gaurav5562 <36435039+gaurav5562@users.noreply.github.com>
2018-03-12 10:17:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-
+var crypto=require('crypto');
 var config = {
     user:'sharmagaurav5562',
     database:'sharmagaurav5562',
@@ -10,6 +10,20 @@ var config = {
     port:5432,
     password: process.env.DB_PASSWORD
 };
+
+
+function hash(input,salt){
+    var hashed=crypt.pbkdf2sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req,params,input,"this-is-random-string");
+    res.send(hashedString);
+});
+
+
+
+
 
 var app = express();
 app.use(morgan('combined'));
